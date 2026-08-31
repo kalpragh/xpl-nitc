74d4b26e17cd49c89af44cb3f41555d80cb385d4
kalpragh <kalpanaraghuram96@gmail.com>
2026-08-31 16:09:02 +0000

    stage4ex1 2d arr

---
+++ stage4/stage4ex1/symboltable.h
@@ -0,0 +1,19 @@
+#ifndef SYMBOLTABLE_H
+#define SYMBOLTABLE_H
+#define TYPE_INT 1
+#define TYPE_STR 2
+struct Gsymbol{
+    char *name;
+    int type;
+    int size;
+    int binding;
+    int isArray;
+    int cols;
+    struct Gsymbol *next;
+};
+
+struct Gsymbol *Lookup(char *name);
+void Install(char *name,int type,int size,int isArray,int cols);
+void PrintSymbolTable();
+
+#endif
---
+++ stage4/stage4ex1/tnode.h
@@ -0,0 +1,33 @@
+#ifndef TNODE_H
+#define TNODE_H
+#define TYPE_NONE -1
+#define TYPE_BOOL 0
+#define TYPE_INT  1
+#define TYPE_STR 2
+
+#define NODE_LE 5
+#define NODE_GE 6
+#define NODE_EQ 7
+#define NODE_NE 8
+#define NODE_ARRAY 9
+#define NODE_ARRAY2D 10
+void writeArrayErrorHandler();
+void writeHeader();
+struct Gsymbol;
+struct tnode {
+    int val;
+    int type;
+    int nodetype;
+    char *varname;
+
+    struct Gsymbol *Gentry;
+
+    struct tnode *left;
+    struct tnode *middle;
+    struct tnode *right;
+};
+
+struct tnode* createtree(int val,int type, int nodetype, char* varname,
+                         struct tnode* l, struct tnode* m,struct tnode* r);
+
+#endif
